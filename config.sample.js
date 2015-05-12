a8216a9f4fc827760b84793d91829ff22c9d48aa
Dominick Pham <dominick.pham@gmail.com>
2015-05-11 23:05:06 -0700

    anon token

--- config.sample.js
+++ config.sample.js
@@ -1,4 +1,7 @@
 module.exports = {
     base_url: 'https://your.website.com/',
-    partner_token: ''
+    partner_token: 'your_partner_token',
+
+    // token expires one month from creation, it should be automatically generated somewhere
+    anonymouse_token: 'your_anonymous_drizly_token'
 };
